f1d898d6d53030fde4d5f729ec3af69243c57495
Admin <ghguguu@jghhh>
2021-11-28 17:15:50 +0200

    Initial Commit

--- src/com/akadatsky/Main.java
+++ src/com/akadatsky/Main.java
@@ -7,19 +7,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Fruit redApple = new Fruit();
+        /*Fruit redApple = new Fruit();
         redApple.setType(Fruit.Type.APPLE);
         redApple.setColor(Color.RED);
         redApple.setWeight(232);
         redApple.setPricePerKg(11.5);
-        System.out.println(redApple);
+        System.out.println(redApple);*/
 
+        Fruit redApple = new Fruit(Fruit.Type.APPLE, Color.RED, 11.5, 232);
         Fruit greenApple = new Fruit(Fruit.Type.APPLE, Color.GREEN, 22.90, 230);
+        Fruit yellowLemon = new Fruit(Fruit.Type.LEMON, Color.YELLOW, 30.90, 228);
+        Fruit yellowBanana = new Fruit(Fruit.Type.BANANA, Color.YELLOW, 28.80, 230);
 
         List<Fruit> fruits = new ArrayList<>();
         fruits.add(redApple);
         fruits.add(greenApple);
-
+        fruits.add(yellowLemon);
+        fruits.add(yellowBanana);
         // lemon  228g, 30.90
         // banana  230g, 28.80
 
@@ -29,7 +33,8 @@ public class Main {
             Fruit fruit = fruits.get(i);
             sum += fruit.getPricePerKg() * fruit.getWeight() / 1000.0;
         }
-        System.out.println(sum);
+        // System.out.println(sum);
+        System.out.printf("Purchase cost: " + "%.2f", sum);
 
     }
 }
